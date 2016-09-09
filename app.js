70f39bfa76efe9a138d1c69e2211f91da94ec97d
redhendery <redhendery@gmail.com>
2016-09-09 12:25:07 +1200

    add npm start script to package.JSON

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express')
 var app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!')
 });
 
 app.listen(3000, () => {
-  console.log('App listening at http://localhost:3000/');
+  console.log('App listening at http://localhost:3000/')
 });
