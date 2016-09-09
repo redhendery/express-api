45a9bf24969344cc7e24031fab623314f986106e
redhendery <redhendery@gmail.com>
2016-09-09 12:17:23 +1200

    update function syntax

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express')
 
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('App listening at http://localhost:3000/');
 });
