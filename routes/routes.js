b56994b6c21e5f8ee2cfaca1c677bf51b0617497
redhendery <redhendery@gmail.com>
2016-09-14 08:02:40 +1200

    add about & contact routes and pages

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,14 @@ const router = express.Router()
 // GET Home Page
 router.get('/', (req, res, next) => {
   res.render('index', {title: 'Kitchen Findr'})
-})
+});
+
+router.get('/about', (req, res) => {
+  res.render('about')
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact')
+});
 
 module.exports = router;
