f24371562c64c3a0b3fbeddd41d1e519482a4f6a
redhendery <redhendery@gmail.com>
2016-09-15 09:04:38 +1200

    add styling

--- app.js
+++ app.js
@@ -24,17 +24,17 @@ const googleMapsClient = require('@google/maps').createClient({
 
 // Error Handling
 
-// catch 404 and forward to error handler
+// 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
+// Error handlers
 
-// development error handler
-// will print stacktrace
+// Development error handler
+// Will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -45,8 +45,8 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production error handler
+// No stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
