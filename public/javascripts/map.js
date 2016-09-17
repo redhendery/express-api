d689c37facd65d4d79eb47e9d66f6ca628e62ef9
redhendery <redhendery@gmail.com>
2016-09-18 08:54:16 +1200

    add JS file & folder

---
+++ public/javascripts/map.js
@@ -0,0 +1,21 @@
+function initMap() {
+  var myLatLng = {lat: -41.295210, lng: 174.783687};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: "23 Kent Terrace, Wellington"
+  })
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Kent Terrace'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker)
+  });
+}
