87eeada2ca86111cfb99abb9e0444513ffee93ac
redhendery <redhendery@gmail.com>
2016-09-26 07:13:17 +1300

    refactor app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const bodyParser = require('body-parser')
 const jsonfile = require('jsonfile')
 
 const routes = require('./routes/routes')
@@ -11,15 +10,12 @@ const app = express()
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
-
 app.use('/', routes);
 
-const file = '/data/data.json'
-jsonfile.readFile(file, function(err, obj) {
+const file = './data/data.json'
+jsonfile.readFile(file, (err, obj) => {
   console.dir("This is the JSON obj: ", obj)
 })
 
-module.exports = app
+module.exports = app;
