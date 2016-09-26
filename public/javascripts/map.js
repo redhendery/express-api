7402895fc5ed2b0135d3cff665b4a8125dac2cdd
redhendery <redhendery@gmail.com>
2016-09-26 19:33:41 +1300

    firetruck red

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -12,7 +12,7 @@ function initialize() {
 }
 
 // Loop through the results array and place a marker for each set of coordinates.
-window.eqfeed_callback = function(results) {
+window.kitchen_callback = function(results) {
   for (var i = 0; i < results.features.length; i++) {
     var coords = results.features[i].geometry.coordinates;
     var latLng = new google.maps.LatLng(coords[1],coords[0]);
