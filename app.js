5bf72caa9a2de5fdfe116338bbffe443432bd7f3
redhendery <redhendery@gmail.com>
2016-09-27 20:18:12 +1300

    refactor routes location, pass in dataObj to app

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 
-const routes = require('./routes/routes')
+const routes = require('./routes')
+const kitchens = require('./data')
 
 const app = express()
 
@@ -10,10 +11,12 @@ const app = express()
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
+app.use('/', routes)
+
+console.log(kitchens)
 
 module.exports = app;
---
+++ data.js
@@ -0,0 +1,17 @@
+const kitchens = {
+    kitchens: [{
+        id: 1,
+        name: 'Asian Food Republic',
+        address: '23 Kent Terrace, Wellington'
+    }, {
+				id: 2,
+				name: 'Cafe DeLux',
+				address: '10 Kent Terrace, Wellington'
+    }, {
+        id: 3,
+        name: 'Westpac Stadium',
+				address: '105 Waterloo Quay, Wellington'
+    }]
+}
+
+module.exports = kitchens;
