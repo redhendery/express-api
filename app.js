f99a2125919d0ada52bc94984f8d1f8724aea3a4
redhendery <redhendery@gmail.com>
2016-09-27 21:39:32 +1300

    get rendering data

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const fs = require('fs')
 
 const routes = require('./routes')
-const kitchens = require('./data')
 
 const app = express()
 
@@ -11,14 +11,18 @@ const app = express()
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use('/', routes)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes)
+fs.readFile(__dirname + '/data.JSON', 'utf8', loadKitchens);
 
-console.log("ID: ", kitchens.id)
-console.log("Name: ", kitchens.name)
-console.log("Address: ", kitchens.address)
+function loadKitchens (err, data) {
+  if(err) throw err
+
+  const kitchenObj = JSON.parse(data)
+  kitchens = kitchenObj
+}
 
 module.exports = app;
